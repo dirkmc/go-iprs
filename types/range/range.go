e4159cd177ac9e86377e81172c5cd03309345350
Dirk McCormick <dirkmdev@gmail.com>
2017-11-30 13:58:15 -0300

    Allow minus infinity and positive infinity in range type

--- types/range/range.go
+++ types/range/range.go
@@ -26,9 +26,18 @@ var ErrExpiredRecord = errors.New("expired record")
 
 var log = logging.Logger("recordstore.types.range")
 
-func NewRecord(pk ci.PrivKey, val path.Path, seq uint64, start time.Time, end time.Time) (*pb.IprsEntry, error) {
-	if start.After(end) {
+func NewRecord(pk ci.PrivKey, val path.Path, seq uint64, start *time.Time, end *time.Time) (*pb.IprsEntry, error) {
+	if start != nil && end != nil && (*start).After(*end) {
 		return nil, ErrRecordTimeRange
+	}
+
+	startFmt := "-∞"
+	if start != nil {
+		startFmt = u.FormatRFC3339(*start)
+	}
+	endFmt := "∞"
+	if end != nil {
+		endFmt = u.FormatRFC3339(*end)
 	}
 
 	entry := new(pb.IprsEntry)
@@ -37,7 +46,7 @@ func NewRecord(pk ci.PrivKey, val path.Path, seq uint64, start time.Time, end ti
 	typ := pb.IprsEntry_TimeRange
 	entry.ValidityType = &typ
 	entry.Sequence = proto.Uint64(seq)
-	entry.Validity = []byte(u.FormatRFC3339(start) + "~" + u.FormatRFC3339(end))
+	entry.Validity = []byte(startFmt + "~" + endFmt)
 
 	sig, err := pk.Sign(types.RecordDataForSig(entry))
 	if err != nil {
@@ -77,12 +86,12 @@ func SelectRecord(recs []*pb.IprsEntry, vals [][]byte) (int, error) {
 			}
 
 			// Best record is the one that's valid to the latest possible moment
-			if t[1].After(bestt[1]) {
+			if t[1] == nil && bestt[1] != nil || (t[1] != nil && bestt[1] != nil && (*t[1]).After(*bestt[1])) {
 				best_i = i
 			} else if t[1] == bestt[1] {
 				// If records are valid until an equal time, best record
 				// is the one that's valid since the longest time in the past
-				if t[0].Before(bestt[0]) {
+				if t[0] == nil && bestt[0] != nil || (t[0] != nil && bestt[0] != nil && (*t[0]).Before(*bestt[0])) {
 					best_i = i
 				} else if t[0] == bestt[0] {
 					// This is just to make sure the selection is deterministic
@@ -100,26 +109,34 @@ func SelectRecord(recs []*pb.IprsEntry, vals [][]byte) (int, error) {
 	return best_i, nil
 }
 
-func timeRange(r *pb.IprsEntry) (*[2]time.Time, error) {
+func timeRange(r *pb.IprsEntry) (*[2]*time.Time, error) {
 	timeRange := strings.Split(string(r.GetValidity()), "~")
 	if len(timeRange) != 2 {
 		return nil, errors.New("Invalid TimeRange record")
 	}
 
-	start, err := u.ParseRFC3339(timeRange[0])
-	if err != nil {
-		return nil, err
+	var startPt *time.Time
+	if timeRange[0] != "-∞" {
+		start, err := u.ParseRFC3339(timeRange[0])
+		if err != nil {
+			return nil, err
+		}
+		startPt = &start
 	}
-	end, err := u.ParseRFC3339(timeRange[1])
-	if err != nil {
-		return nil, err
+	var endPt *time.Time
+	if timeRange[1] != "∞" {
+		end, err := u.ParseRFC3339(timeRange[1])
+		if err != nil {
+			return nil, err
+		}
+		endPt = &end
 	}
-
-	if start.After(end) {
+	
+	if startPt != nil && endPt != nil && (*startPt).After(*endPt) {
 		return nil, ErrRecordTimeRange
 	}
 
-	return &[2]time.Time{start, end}, nil
+	return &[2]*time.Time{startPt, endPt}, nil
 }
 
 // ValidateRecord implements ValidatorFunc and verifies that the
@@ -130,10 +147,10 @@ func ValidateRecord(k string, entry *pb.IprsEntry) error {
 		log.Debug("failed parsing Iprs record Time Range")
 		return err
 	}
-	if time.Now().Before(t[0]) {
+	if t[0] != nil && time.Now().Before(*t[0]) {
 		return ErrPendingRecord
 	}
-	if time.Now().After(t[1]) {
+	if t[1] != nil && time.Now().After(*t[1]) {
 		return ErrExpiredRecord
 	}
 	return nil
--- types/range/range_test.go
+++ types/range/range_test.go
@@ -11,7 +11,11 @@ import (
 )
 
 func TestNewRecord(t *testing.T) {
+	var BeginningOfTime *time.Time
+	var EndOfTime *time.Time
 	ts := time.Now()
+	InOneHour := ts.Add(time.Hour)
+	OneHourAgo := ts.Add(time.Hour * -1)
 
 	// generate a key for signing the records
 	r := u.NewSeededRand(15) // generate deterministic keypair
@@ -21,27 +25,48 @@ func TestNewRecord(t *testing.T) {
 	}
 
 	// Start before end OK
-	_, err = NewRecord(priv, path.Path("foo"), 1, ts, ts.Add(time.Hour))
+	_, err = NewRecord(priv, path.Path("foo"), 1, &ts, &InOneHour)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewRecord(priv, path.Path("foo"), 1, BeginningOfTime, &ts)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewRecord(priv, path.Path("foo"), 1, &ts, EndOfTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	_, err = NewRecord(priv, path.Path("foo"), 1, BeginningOfTime, EndOfTime)
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	// End before start FAIL
-	_, err = NewRecord(priv, path.Path("bar"), 1, ts.Add(time.Hour), ts.Add(time.Hour * -1))
+	_, err = NewRecord(priv, path.Path("bar"), 1, &InOneHour, &OneHourAgo)
 	if err == nil {
 		t.Fatal("Expected end before start error")
 	}
 
 	// Start equals end OK
-	_, err = NewRecord(priv, path.Path("baz"), 1, ts, ts)
+	_, err = NewRecord(priv, path.Path("baz"), 1, &ts, &ts)
 	if err != nil {
 		t.Fatal(err)
 	}
 }
 
 func TestOrdering(t *testing.T) {
+	var BeginningOfTime *time.Time
+	var EndOfTime *time.Time
 	// select timestamp so selection is deterministic
 	ts := time.Unix(1000000, 0)
+	InOneHour := ts.Add(time.Hour)
+	OneHourAgo := ts.Add(time.Hour * -1)
+	InTwoHours := ts.Add(time.Hour * 2)
+	InThreeHours := ts.Add(time.Hour * 3)
 
 	// generate a key for signing the records
 	r := u.NewSeededRand(15) // generate deterministic keypair
@@ -50,37 +75,47 @@ func TestOrdering(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	e1, err := NewRecord(priv, path.Path("foo"), 1, ts, ts.Add(time.Hour))
+	e1, err := NewRecord(priv, path.Path("foo"), 1, &ts, &InOneHour)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e2, err := NewRecord(priv, path.Path("bar"), 2, ts, ts.Add(time.Hour))
+	e2, err := NewRecord(priv, path.Path("bar"), 2, &ts, &InOneHour)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e3, err := NewRecord(priv, path.Path("baz"), 3, ts, ts.Add(time.Hour))
+	e3, err := NewRecord(priv, path.Path("baz"), 3, &ts, &InOneHour)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e4, err := NewRecord(priv, path.Path("cat"), 3, ts, ts.Add(time.Hour*2))
+	e4, err := NewRecord(priv, path.Path("cat"), 3, &ts, &InTwoHours)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e5, err := NewRecord(priv, path.Path("dog"), 4, ts, ts.Add(time.Hour*3))
+	e5, err := NewRecord(priv, path.Path("dog"), 4, &ts, &InThreeHours)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e6, err := NewRecord(priv, path.Path("fish"), 4, ts.Add(time.Hour*-1), ts.Add(time.Hour*3))
+	e6, err := NewRecord(priv, path.Path("fish"), 4, &OneHourAgo, &InThreeHours)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e7, err := NewRecord(priv, path.Path("goat"), 4, ts.Add(time.Hour*-1), ts.Add(time.Hour*3))
+	e7, err := NewRecord(priv, path.Path("goat"), 4, &OneHourAgo, EndOfTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e8, err := NewRecord(priv, path.Path("hornet"), 4, BeginningOfTime, EndOfTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	e9, err := NewRecord(priv, path.Path("impala"), 4, BeginningOfTime, EndOfTime)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -121,17 +156,33 @@ func TestOrdering(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	// e7 should be selected as its signauture will win in the comparison
+	// e7 has the higest expiration and lowest start date
 	err = AssertSelected(e7, e1, e2, e3, e4, e5, e6, e7)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	_ = []interface{}{e1, e2, e3, e4, e5, e6, e7}
+	// e8 has the higest expiration and lowest start date
+	err = AssertSelected(e7, e1, e2, e3, e4, e5, e6, e7, e8)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// e9 should be selected as its signauture will win in the comparison
+	err = AssertSelected(e7, e1, e2, e3, e4, e5, e6, e7, e8, e9)
+	if err != nil {
+		t.Fatal(err)
+	}
 }
 
 func TestValidation(t *testing.T) {
+	var BeginningOfTime *time.Time
+	var EndOfTime *time.Time
 	ts := time.Now()
+	OneHourAgo := ts.Add(time.Hour * -1)
+	TwoHoursAgo := ts.Add(time.Hour * -2)
+	InTwoHours := ts.Add(time.Hour * 2)
+	InOneHour := ts.Add(time.Hour)
 
 	// generate a key for signing the records
 	r := u.NewSeededRand(15) // generate deterministic keypair
@@ -140,32 +191,78 @@ func TestValidation(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	e1, err := NewRecord(priv, path.Path("foo"), 1, ts.Add(time.Hour * -2), ts.Add(time.Hour * -1))
+	pendingA, err := NewRecord(priv, path.Path("foo"), 1, &TwoHoursAgo, &OneHourAgo)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e2, err := NewRecord(priv, path.Path("bar"), 1, ts.Add(time.Hour * -1), ts.Add(time.Hour))
+	pendingB, err := NewRecord(priv, path.Path("foo2"), 1, BeginningOfTime, &OneHourAgo)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	e3, err := NewRecord(priv, path.Path("baz"), 1, ts.Add(time.Hour * 1), ts.Add(time.Hour * 2))
+	okA, err := NewRecord(priv, path.Path("bar"), 1, &OneHourAgo, &InOneHour)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = ValidateRecord("foo", e1)
+	okB, err := NewRecord(priv, path.Path("bar2"), 1, BeginningOfTime, &InOneHour)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	okC, err := NewRecord(priv, path.Path("bar3"), 1, &OneHourAgo, EndOfTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	okD, err := NewRecord(priv, path.Path("bar4"), 1, BeginningOfTime, EndOfTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expiredA, err := NewRecord(priv, path.Path("baz"), 1, &InOneHour, &InTwoHours)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	expiredB, err := NewRecord(priv, path.Path("baz2"), 1, &InOneHour, EndOfTime)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+
+	err = ValidateRecord("foo", pendingA)
+	if err == nil {
+		t.Fatal("Expected pending error")
+	}
+	err = ValidateRecord("foo2", pendingB)
 	if err == nil {
 		t.Fatal("Expected pending error")
 	}
 
-	err = ValidateRecord("bar", e2)
+	err = ValidateRecord("bar", okA)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ValidateRecord("bar2", okB)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ValidateRecord("bar3", okC)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = ValidateRecord("bar4", okD)
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	err = ValidateRecord("foo", e3)
+	err = ValidateRecord("baz", expiredA)
+	if err == nil {
+		t.Fatal("Expected expired error")
+	}
+	err = ValidateRecord("baz2", expiredB)
 	if err == nil {
 		t.Fatal("Expected expired error")
 	}
